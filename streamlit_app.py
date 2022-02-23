17c1e6280249a0a555fa9116b4532c5c1ba15c17
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2022-02-23 19:55:42 +0530

    added sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,25 @@ from tensorflow.keras import preprocessing
 import time
 from fruit import Fruit
 
-st.title("Fruit Classification using TensorflowLite for microcontrollers")
-st.header("Fruit Classification Example")
-st.text("Upload Fruit Image for classification")
+st.title("Fruit Detection using TensorflowLite for microcontrollers")
+st.header("Fruit Detection Example")
+st.text("Upload Fruit Image for detection 🍎")
 st.write("")
 
 
-st.info("You can refer to [this]('https://www.kaggle.com/moltean/fruits') dataset for fruit images")
-
+st.info("You can refer to [this]('https://www.kaggle.com/moltean/fruits') dataset for fruit images.")
+st.write('---')
 # uploaded_file = st.file_uploader("Upload fruit image", type=["jpg","png","jpeg"])
 
-        
+st.sidebar.header("🍇Fruit Detection using TensorflowLite for microcontrollers")
+st.sidebar.write('---')
+st.sidebar.subheader("A tensorflowLite model for fruit classification made to be used on microcontrollers like Arduino Nano BLE33 or ESP32 and trained on [Fruits_360](https://www.kaggle.com/moltean/fruits) dataset.")
+st.sidebar.subheader(" You can check out source code of this website [here](https://github.com/ujjwal404/fruitClassificationTFLite).")
+st.sidebar.write('---')
+st.sidebar.markdown("Made by *[@UjjwalKadam](https://github.com/ujjwal404), [@TarushBajaj](https://github.com/Tarushfx)* and *[@Ujjwal](https://github.com/ujjwal)*")
+
+
+
 def predict(image):
     test_image = image.resize((100,100))
     test_image = preprocessing.image.img_to_array(test_image)
@@ -42,6 +50,7 @@ def main():
     
     if class_btn:
         if file_uploaded is None:
+            st.write("")
             st.write("Invalid command, please upload an image")
         else:
             with st.spinner('Model working....'):
