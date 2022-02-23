9c99682b7c067263fb898b3ebbb728ad156a2455
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2022-02-23 18:43:34 +0530

    init commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from cgi import test
+from difflib import restore
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.models import *
+from tensorflow.keras import preprocessing
+import time
+from fruit import Fruit
+
+st.title("Fruit Classification using TensorflowLite for microcontrollers")
+st.header("Fruit Classification Example")
+st.text("Upload Fruit Image for classification")
+st.write("")
+
+
+st.info("You can refer to [this]('https://www.kaggle.com/moltean/fruits') dataset for fruit images")
+
+# uploaded_file = st.file_uploader("Upload fruit image", type=["jpg","png","jpeg"])
+
+        
+def predict(image):
+    test_image = image.resize((100,100))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    
+    obj = Fruit(test_image)
+    result = obj.predictLite()
+    return result
+  
+
+def main():
+    file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
+    
+    if file_uploaded is not None:    
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+    class_btn = st.button("Classify")    
+    
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                 plt.imshow(image)
+                 plt.axis("off")
+                 predictions = predict(image)
+                 time.sleep(1)
+                 st.success('Classified')
+                 st.write(predictions)
+        
+main()
+        
---
+++ fruit.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import *
+import numpy as np
+
+class Fruit:
+    
+    def __init__(self, img = ''):
+        self.img = img
+        self.model = load_model('Fruits_360.h5')
+        self.Label_dict = labels =  {'Apple Braeburn': 0,
+             'Apple Golden': 1,
+             'Apple Granny Smith': 2,
+             'Apple Red': 3,
+             'Apricot': 4,
+             'Avocado': 5,
+             'Avocado ripe': 6,
+             'Banana': 7,
+             'Banana Lady Finger': 8,
+             'Banana Red': 9,
+             'Cactus fruit': 10,
+             'Cantaloupe 1': 11,
+             'Cantaloupe 2': 12,
+             'Carambula': 13,
+             'Cherry 1': 14,
+             'Cherry Wax Black': 15,
+             'Cherry Wax Red': 16,
+             'Cherry Wax Yellow': 17,
+             'Chestnut': 18,
+             'Clementine': 19,
+             'Cocos': 20,
+             'Dates': 21,
+             'Grape Blue': 22,
+             'Grape Pink': 23,
+             'Grape White': 24,
+             'Grapefruit Pink': 25,
+             'Grapefruit White': 26,
+             'Guava': 27,
+             'Hazelnut': 28,
+             'Huckleberry': 29,
+             'Kaki': 30,
+             'Kiwi': 31,
+             'Kumquats': 32,
+             'Lemon': 33,
+             'Lemon Meyer': 34,
+             'Limes': 35,
+             'Lychee': 36,
+             'Mandarine': 37,
+             'Mango': 38,
+             'Mangostan': 39,
+             'Melon Piel de Sapo': 40,
+             'Mulberry': 41,
+             'Nectarine': 42,
+             'Orange': 43,
+             'Papaya': 44,
+             'Passion Fruit': 45,
+             'Peach': 46,
+             'Peach 2': 47,
+             'Peach Flat': 48,
+             'Pear': 49,
+             'Pear Kaiser': 50,
+             'Pineapple': 51,
+             'Pineapple Mini': 52,
+             'Pitahaya Red': 53,
+             'Plum': 54,
+             'Plum 2': 55,
+             'Plum 3': 56,
+             'Pomegranate': 57,
+             'Pomelo Sweetie': 58,
+             'Rambutan': 59,
+             'Raspberry': 60,
+             'Redcurrant': 61,
+             'Strawberry': 62,
+             'Strawberry Wedge': 63,
+             'Tomato 1': 64,
+             'Tomato 2': 65,
+             'Tomato 4': 66,
+             'Tomato Cherry Red': 67,
+             'Tomato Maroon': 68,
+             'Walnut': 69}
+        self.label = list(self.Label_dict.keys())
+    
+    def predict(self):
+        result=self.model.predict(self.img)
+        result_list = result[0].tolist()
+        result_class = self.label[result_list.index(max(result_list))]
+        return result_class
+      
+    def predictLite(self):
+        model = "fruits.tflite"
+        interpreter = tf.lite.Interpreter(model_path = model)
+        interpreter.allocate_tensors()
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        
+        input_shape = input_details[0]['shape']
+        interpreter.set_tensor(input_details[0]['index'], self.img)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        result_list = output_data[0].tolist()
+        result_class = self.label[result_list.index(max(result_list))]
+        return result_class
+        
